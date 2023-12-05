b97ca249f83a61cd5c8d4e8e81495c9abb39cc6e
HollaNotes <noellewidick@gmail.com>
2023-12-04 21:26:42 -0800

    pushing everything out

---
+++ Project_3_Schema.sql
@@ -0,0 +1,17 @@
+DROP TABLE IF EXISTS real_estate;
+CREATE TABLE real_estate (
+	status VARCHAR,
+	bed NUMERIC,
+	bath NUMERIC,
+	acre_lot NUMERIC,
+	city VARCHAR,
+	state VARCHAR,
+	zip_code NUMERIC,
+	house_size NUMERIC,
+	prev_sold_date NUMERIC,
+	price NUMERIC 
+);
+	
+	
+
+
