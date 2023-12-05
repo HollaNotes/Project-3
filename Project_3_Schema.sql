3e8f2eaff0c316e1873970b563f7542187a4482c
HollaNotes <noellewidick@gmail.com>
2023-12-04 22:19:55 -0800

    Fixed Project_3_Schema

--- Project_3_Schema.sql
+++ Project_3_Schema.sql
@@ -1,14 +1,15 @@
-DROP TABLE IF EXISTS real_estate;
-CREATE TABLE real_estate (
+drop table if exists real_estate;
+
+CREATE TABLE real_estate(
 	status VARCHAR,
 	bed NUMERIC,
 	bath NUMERIC,
 	acre_lot NUMERIC,
 	city VARCHAR,
 	state VARCHAR,
-	zip_code NUMERIC,
+	zip_code VARCHAR,
 	house_size NUMERIC,
-	prev_sold_date NUMERIC,
+	prev_sold_date DATE,
 	price NUMERIC 
 );
 	
