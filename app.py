ef527694c0625388b58dc04f520e6b68ad554153
HollaNotes <noellewidick@gmail.com>
2023-12-07 21:35:10 -0800

    saving after class

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, jsonify
 
-# Dictionary of Justice League
 justice_league_members = [
     {"superhero": "Aquaman", "real_name": "Arthur Curry"},
     {"superhero": "Batman", "real_name": "Bruce Wayne"},
@@ -21,7 +20,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
-@app.route("/api/v1.0/justice-league")
+@app.route("/api/v1.0/")
 def justice_league():
     """Return the justice league data as json"""
 
@@ -33,8 +32,40 @@ def welcome():
     return (
         f"Welcome to the Justice League API!<br/>"
         f"Available Routes:<br/>"
-        f"/api/v1.0/justice-league"
+        f"/api/v1.0/justice-league<br/>"
+        f"/api/v1.0/justice-league/superhero/batman<br/>"
+        f"/api/v1.0/justice-league/real_name/bruce%20wayne"
     )
+
+
+@app.route("/api/v1.0/justice-league/real_name/<real_name>")
+def justice_league_by_real_name(real_name):
+    """Fetch the Justice League character whose real_name matches
+       the path variable supplied by the user, or a 404 if not."""
+
+    canonicalized = real_name.replace(" ", "").lower()
+    for character in justice_league_members:
+        search_term = character["real_name"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return jsonify(character)
+
+    return jsonify({"error": f"Character with real_name {real_name} not found."}), 404
+
+
+@app.route("/api/v1.0/justice-league/superhero/<superhero>")
+def justice_league_by_superhero__name(superhero):
+    """Fetch the Justice League character whose superhero matches
+       the path variable supplied by the user, or a 404 if not."""
+
+    canonicalized = superhero.replace(" ", "").lower()
+    for character in justice_league_members:
+        search_term = character["superhero"].replace(" ", "").lower()
+
+        if search_term == canonicalized:
+            return jsonify(character)
+
+    return jsonify({"error": "Character not found."}), 404
 
 
 if __name__ == "__main__":
