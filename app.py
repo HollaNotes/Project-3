2380699332f811094363d936f1eb19ee56abff22
HollaNotes <noellewidick@gmail.com>
2023-12-10 17:04:15 -0800

    saving

--- app.py
+++ app.py
@@ -1,272 +1,170 @@
-import pandas as pd
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
 from flask import Flask, jsonify
+import psycopg2
 
-
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("postgresql://postgres:password@localhost:5432/Project 3")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-
-# Save reference to the table
-realestateca = Base.classes.filter6
-
-#################################################
-# Flask Setup
-#################################################
 app = Flask(__name__)
 
+# Create a function to fetch data and jsonify it
+def jsonify_all_data():
+    conn = psycopg2.connect(database="Project 3", user="postgres", password="password", host="localhost", port="5432")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM filter6")
+    users = cursor.fetchall()
+
+    column_names = [desc[0] for desc in cursor.description]  # Fetch column names
+
+    cursor.close()
+    conn.close()
+
+    # Create a list to store dictionaries
+    users_list = []
+
+    # Convert each row tuple to a dictionary
+    for row in users:
+        user_dict = {}
+        for idx, column in enumerate(column_names):
+            user_dict[column] = row[idx]
+        users_list.append(user_dict)
+
+    return jsonify(users_list)
+
+# Create a route to access the function
+@app.route("/", methods=["GET"])
+def get_users():
+    return jsonify_all_data()
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+# import pandas as pd
+# import numpy as np
+# from flask import Flask, jsonify
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, inspect, text
+# from pathlib import Path
+# import pandas as pd
+# from sqlalchemy import Column, Integer, String, Float
+# from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy import create_engine, MetaData, Table, Column, ForeignKey
+# from sqlalchemy.ext.automap import automap_base
 
 
 
-#################################################
-# Flask Routes
-#################################################
+# #################################################
+# # Database Setup
+# #################################################
+# engine = create_engine("postgresql://postgres:password@localhost:5432/Project 3")
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+# conn = engine.connect()
+
+
+# # Save reference to the table
+# realestateca = Base.classes.filter6
+
+# #################################################
+# # Flask Setup
+# #################################################
+# app = Flask(__name__)
 
 
 # @app.route("/")
-# def welcome():
-#     return (
-#         f"PLEASE PLEASE PLEASE WORK<br/>"
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/DATA"
-#     )
-
-
-
-
-@app.route("/")
-def data():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return a list of all passenger names"""
-    # Query all passengers
-    results = session.query(realestateca.temp_index).all()
-    # results = session.query(realestateca.temp_index,  \
-    #                         realestateca.event, realestateca.price, \
-    #                             realestateca.city, realestateca.state, realestateca.yearBuilt, \
-    #                                 realestateca.streetAddress, realestateca.zipcode, realestateca.longitude, \
-    #                                     realestateca.latitude, realestateca.description, \
-    #                                         realestateca.currency,  realestateca.livingArea, realestateca.livingAreaValue, \
-    #                                             realestateca.lotAreaUnits,   realestateca.bathrooms,realestateca.bedrooms, \
-    #                                                 realestateca.buildingArea, realestateca.parking,  realestateca.garageSpaces, \
-    #                                                     realestateca.hasGarage, realestateca.levels, realestateca.pool, realestateca.spa, \
-    #                                                         realestateca.isNewConstruction, realestateca.hasPetsAllowed, realestateca.homeType, realestateca.county).all()
-
-    session.close()
-
-    # Convert list of tuples into normal list
-    realestateresults = list(np.ravel(results))
-
-    return jsonify(realestateresults)
-
-
-# @app.route("/api/v1.0/descriptions")
-# def descriptions():
+# def data():
 #     # Create our session (link) from Python to the DB
 #     session = Session(engine)
-
-#     """Return a list of all passenger names"""
-#     # Query all passengers
-#     results = session.query(realestateca2.description).all()
+#     data = pd.read_sql("SELECT * FROM filter6", conn)
 
 #     session.close()
 
 #     # Convert list of tuples into normal list
-#     all_desc = list(np.ravel(results))
+#     realestateresults = list(np.ravel(data))
 
-#     return jsonify(all_desc)
+#     return jsonify(realestateresults)
 
 
-# @app.route("/api/v1.0/passengers")
-# def passengers():
+
+
+# import pandas as pd
+# import numpy as np
+# from flask import Flask, jsonify
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from sqlalchemy.ext.automap import automap_base
+
+# engine = create_engine("postgresql://postgres:password@localhost:5432/Project 3")
+
+# # reflect an existing database into a new model
+# Base = automap_base()
+# # reflect the tables
+# Base.prepare(engine, reflect=True)
+
+# # Save reference to the table
+# realestateca = Base.classes.filter6
+
+# #################################################
+# # Flask Setup
+# #################################################
+# app = Flask(__name__)
+
+# @app.route("/")
+# def data():
 #     # Create our session (link) from Python to the DB
-#     session = Session(engine)
+#     Session = sessionmaker(bind=engine)
+#     session = Session()
 
-#     """Return a list of passenger data including the name, age, and sex of each passenger"""
-#     # Query all passengers
-#     results = session.query(realestateca2.price, realestateca2.yearBuilt, realestateca2.county).all()
+#     # Query all data from the table
+#     data = session.query(realestateca).all()
 
 #     session.close()
 
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_desc = []
-#     for name, age, sex in results:
-#         listing_dict = {}
-#         listing_dict["name"] = name
-#         listing_dict["age"] = age
-#         listing_dict["sex"] = sex
-#         all_desc.append(listing_dict)
-
-#     return jsonify(all_desc)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
-import pandas as pd
-import numpy as np
-
-import sqlalchemy
-from sqlalchemy.ext.automap import automap_base
-from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-
-from flask import Flask, jsonify
-
-
-#################################################
-# Database Setup
-#################################################
-engine = create_engine("postgresql://postgres:password@localhost:5432/Project 3")
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(engine, reflect=True)
-
-# Save reference to the table
-realestateca = Base.classes.realestateca2
-
-#################################################
-# Flask Setup
-#################################################
-app = Flask(__name__)
-
-
-
-#################################################
-# Flask Routes
-#################################################
-
-@app.route("/")
-def welcome():
-    return (
-        f"PLEASE PLEASE PLEASE WORK<br/>"
-        f"Available Routes:<br/>"
-        f"/api/v1.0/DATA"
-    )
-
-
-
-@app.route("/api/v1.0/DATA")
-def data():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    # Query all passengers
-    results = session.query(realestateca.name).all()
-
-    # Convert list of tuples into normal list
-    realestateresults = list(np.ravel(results))
-
-    return jsonify(realestateresults)
-
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
-    # # Query all records
-    # results = session.query(realestateca.temp_index, realestateca.id, realestateca.stateId, realestateca.countyId, realestateca.cityId, realestateca.country, realestateca.datePostedString, realestateca.is_bankOwned, realestateca.is_forAuction, realestateca.event, realestateca.time, realestateca.price, realestateca.pricePerSquareFoot, realestateca.city, realestateca.state, realestateca.yearBuilt, realestateca.streetAddress, realestateca.zipcode, realestateca.longitude, realestateca.latitude, realestateca.hasBadGeocode, realestateca.description, realestateca.currency,  realestateca.livingArea, realestateca.livingAreaValue, realestateca.lotAreaUnits,   realestateca.bathrooms,realestateca.bedrooms, realestateca.buildingArea, realestateca.parking,  realestateca.garageSpaces, realestateca.hasGarage, realestateca.levels, realestateca.pool, realestateca.spa, realestateca.isNewConstruction, realestateca.hasPetsAllowed, realestateca.homeType, realestateca.county).all() 
-
-    # session.close()
-
-    # all_records = []
-    # for temp_index, id, stateId, countyId, cityId, country, datePostedString, is_bankOwned, is_forAuction,  event,  time,   price,  pricePerSquareFoot, city,   state,  yearBuilt,  streetAddress,  zipcode,    longitude,  latitude,   hasBadGeocode,  description,    currency,   livingArea, livingAreaValue,    lotAreaUnits,   bathrooms,  bedrooms,   buildingArea,   parking, garageSpaces,  hasGarage,  levels, pool, spa, isNewConstruction, hasPetsAllowed,   homeType, county in results:
-    #     all_records_dict = {}
-    #     all_records_dict["temp_index"] = temp_index
-    #     all_records_dict["id"] = id
-    #     all_records_dict["stateId"] = stateId
-    #     all_records_dict["countyId"] = countyId
-    #     all_records_dict["cityId"] =  cityId  
-    #     all_records_dict["country_d"] = country
-    #     all_records_dict["datePostedString"] = datePostedString   
-    #     all_records_dict["is_bankOwned"] = is_bankOwned   
-    #     all_records_dict["is_forAuction"] =   is_forAuction
-    #     all_records_dict["event"] =   event
-    #     all_records_dict["time"] = time   
-    #     all_records_dict["price"] =   price
-    #     all_records_dict["pricePerSquareFoot"] = pricePerSquareFoot
-    #     all_records_dict["city"] = city   
-    #     all_records_dict["state"] = state 
-    #     all_records_dict["yearBuilt"] = yearBuilt
-    #     all_records_dict["streetAddress"] = streetAddress    
-    #     all_records_dict["zipcode"] = zipcode
-    #     all_records_dict["longitude"] =   longitude
-    #     all_records_dict["latitude"] = latitude   
-    #     all_records_dict["hasBadGeocode"] = hasBadGeocode 
-    #     all_records_dict["description"] = description 
-    #     all_records_dict["currency"] = currency   
-    #     all_records_dict["livingArea"] = livingArea   
-    #     all_records_dict["livingAreaValue"] = livingAreaValue
-    #     all_records_dict["lotAreaUnits"] = lotAreaUnits
-    #     all_records_dict["bathrooms"] =   bathrooms
-    #     all_records_dict["bedrooms"] = bedrooms   
-    #     all_records_dict["buildingArea"] = buildingArea
-    #     all_records_dict["parking"] = parking 
-    #     all_records_dict["garageSpaces"] = garageSpaces   
-    #     all_records_dict["hasGarage"] = hasGarage
-    #     all_records_dict["levels"] = levels   
-    #     all_records_dict["pool"] = pool   
-    #     all_records_dict["spa"] = spa
-    #     all_records_dict["isNewConstruction"] = isNewConstruction
-    #     all_records_dict["hasPetsAllowed"]    = hasPetsAllowed
-    #     all_records_dict["homeType"] = homeType   
-    #     all_records_dict["county"] = county
-    # all_records.append(all_records_dict)
-
-    # return jsonify(all_records)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#     # Convert query result to a list of dictionaries
+#     realestateresults = []
+#     for row in data:
+#         row_dict = {
+#             "temp_index": row.temp_index,
+#             "event": row.event,
+#             "price": row.price,
+#             "city": row.city,
+#             "datePostedString": row.datePostedString,   
+#             "event": row.event,
+#             "price": row.price,
+#             "pricePerSquareFoot": row.pricePerSquareFoot,
+#             "city": row.city, 
+#             "state": row.state,
+#             "yearBuilt": row.yearBuilt,
+#             "streetAddress": row.streetAddress,   
+#             "zipcode": row.zipcode,
+#             "longitude": row.longitude,
+#             "latitude": row.latitude,   
+#             "description":  row.description,
+#             "currency":  row.currency,   
+#             "livingArea":  row.livingArea,   
+#             "livingAreaValue":  row.livingAreaValue,
+#             "lotAreaUnits":  row.lotAreaUnits,
+#             "bathrooms":    row.bathrooms,
+#             "bedrooms":  row.bedrooms,  
+#             "buildingArea": row.buildingArea,
+#             "parking": row.parking,
+#             "garageSpaces": row.garageSpaces,   
+#             "hasGarage": row.hasGarage,
+#             "levels": row.levels,  
+#             "pool": row.pool,  
+#             "spa": row.spa,
+#             "isNewConstruction": row.isNewConstruction,
+#             "hasPetsAllowed": row.hasPetsAllowed,
+#             "homeType": row.homeType,   
+#             "county": row.county
+#         }
+#         realestateresults.append(row_dict)
+
+#     return jsonify(realestateresults)
+
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
