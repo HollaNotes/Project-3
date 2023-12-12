8dddcd6d9fcd63021d387df6e99f2c87ae13a9c9
HollaNotes <noellewidick@gmail.com>
2023-12-12 11:16:24 -0800

    adding in any files that I haven't pushed yet

--- summary_table.js
+++ summary_table.js
@@ -1,11 +1,48 @@
-// BRING IN THE DATA
-// Get the samples endpoint
 const url = "https://raw.githubusercontent.com/HollaNotes/Project-3/main/users_data.json";
 
-// Fetch the JSON data and console log it
-d3.json(url).then(function(data) {
-    console.log("Data: ", data);
-    
-});    
+function init() {
+    // Fetch the JSON data
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            console.log("Data: ", data);
 
+            // Extracting data for plot
+            let prices = data.map(object => parseFloat(object.price));
+            let bedrooms = data.map(object => parseFloat(object.bedrooms));
 
+            // Create trace for Plotly
+            let trace1 = {
+                x: prices,
+                y: bedrooms,
+                name: "table",
+                type: "bar",
+            };
+
+            // Data array
+            let plotData = [trace1];
+
+            // Apply a title to the layout and add axis labels
+            let layout = {
+                title: "Bar Chart",
+                margin: {
+                    l: 100,
+                    r: 100,
+                    t: 100,
+                    b: 100
+                },
+                xaxis: {
+                    title: 'Prices' // X-axis label
+                },
+                yaxis: {
+                    title: 'Bedrooms' // Y-axis label
+                }
+            };
+
+            // Create the Plotly plot
+            Plotly.newPlot("bar", plotData, layout);
+        })
+        .catch(error => console.error("Error fetching data:", error));
+}
+
+init();
