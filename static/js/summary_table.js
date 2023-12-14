461a1c8098def90f6ad6e2480555695ebd52ebdf
HollaNotes <noellewidick@gmail.com>
2023-12-14 09:51:05 -0800

    saving - still not working

--- static/js/summary_table.js
+++ static/js/summary_table.js
@@ -1,58 +1,37 @@
-const url = "https://raw.githubusercontent.com/HollaNotes/Project-3/main/static/data/realEstate_data.json";
+let url = "https://raw.githubusercontent.com/HollaNotes/Project-3/main/static/data/realEstate_data.json";
 
-function summary_table() {
-    // Fetch the JSON data
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log("Data: ", data);
+let city_i = document.getElementById('city');
+let county_i = document.getElementById('county');
+let price_i = document.getElementById('price');
+let sqft_i = document.getElementById('sqft');
+let price_sqt_i = document.getElementById('price_sqt_i');
 
-            // Extracting data for plot
-            let prices = data.map(object => parseFloat(object.price));
-            let bedrooms = data.map(object => parseFloat(object.bedrooms));
-            let pricepersquarefoot = data.map(object => parseFloat(object.pricepersquarefoot));
-            let event = data.map(object => parseFloat(object.event));
-            let yearbuilt = data.map(object => parseFloat(object.yearbuilt));
-            let zipcode = data.map(object => parseFloat(object.zipcode));
-            let streetaddress = data.map(object => parseFloat(object.streetaddress));
-            let countyid = data.map(object => parseFloat(object.countyid));
-            let sqfoot = data.map(object => parseFloat(object.pricepersquarefoot));
-            let county = data.map(object => parseFloat(object.county));
+    
+function sumtbl(properties) {
+    d3.json(url).then(function(data){
+        // Set variable for metadata of samples
+        let city = data.city;
+        let county = data.county;
+        let price = data.price;
+        let sqft = data.livingarea;
+        let price_sqft = data.pricepersquarefoot;
+        console.log("hellp")
+        
+        
 
-            // Create trace for Plotly
-            let trace1 = {
-                x: prices,
-                y: bedrooms,
-                name: "table",
-                type: "bar",
-            };
 
-            // Data array
 
-            
-            let plotData = [trace1];
 
-            // Apply a title to the layout and add axis labels
-            let layout = {
-                title: "Bar Chart",
-                margin: {
-                    l: 100,
-                    r: 100,
-                    t: 100,
-                    b: 100
-                },
-                xaxis: {
-                    title: 'Prices' // X-axis label
-                },
-                yaxis: {
-                    title: 'Bedrooms' // Y-axis label
-                }
-            };
 
-            // Create the Plotly plot
-            Plotly.newPlot("bar", plotData, layout);
-        })
-        .catch(error => console.error("Error fetching data:", error));
+
+})};
+
+function avg(input) {
+    let total = 0;
+    for (let i = 0; i < input.length; i++) {
+        total += input[i];
+    }
+    let meanValue = total / input.length;
+    return meanValue;
+
 }
-
-summary_table();
